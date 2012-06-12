abb502b1bb55480771b21600062782ba5d6b343d
Peter Eysermans <peter@twototango.be>
2012-06-12 19:03:29 +0200

    Added MSpec library, added MSpec tests for a running dog, files were not added in previous commit

---
+++ code/ADesignPatternADay/ADesignPatternADay.Tests/Decorator/DogTestFixture.cs
@@ -0,0 +1,23 @@
+﻿using ADesignPatternADay.Decorator;
+using Machine.Specifications;
+
+namespace ADesignPatternADay.Tests.Decorator
+{
+    [Subject("Measuring the speed of a running dog")]
+    public class When_measuring_the_speed_of_a_running_dog
+    {
+        Establish context = () =>
+        {
+            _classUnderTest = new Dog();
+        };
+
+        Because of = () =>
+            _measuredSpeed = _classUnderTest.Run();
+
+        It should_have_a_speed_of_25_miles_per_hour = () =>
+            _measuredSpeed.ShouldEqual(25);
+
+        private static Dog _classUnderTest;
+        private static int _measuredSpeed;
+    }
+}
