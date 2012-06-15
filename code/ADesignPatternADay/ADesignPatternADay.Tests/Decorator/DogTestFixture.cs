22b5bd50a42576fee8eb227166bcea5f7d6e8f02
Peter Eysermans <peter@twototango.be>
2012-06-15 22:13:02 +0200

    Added test case for a handicapped dog with bionic legs

--- code/ADesignPatternADay/ADesignPatternADay.Tests/Decorator/DogTestFixture.cs
+++ code/ADesignPatternADay/ADesignPatternADay.Tests/Decorator/DogTestFixture.cs
@@ -1,8 +1,27 @@
 ﻿using ADesignPatternADay.Decorator;
+using ADesignPatternADay.Decorator.Decorators;
 using Machine.Specifications;
 
 namespace ADesignPatternADay.Tests.Decorator
 {
+    [Subject("Measuring the speed of a running handicapped dog with bionic legs")]
+    public class When_measuring_the_speed_of_a_running_handicapped_dog_with_bionic_legs
+    {
+        Establish context = () =>
+        {
+            _classUnderTest = new BionicLegsDecorator(new HandicappedDecorator(new Dog()));
+        };
+
+        Because of = () =>
+            _measuredSpeed = _classUnderTest.Run();
+
+        It should_have_a_speed_of_16_miles_per_hour = () =>
+            _measuredSpeed.ShouldEqual(16);
+
+        private static BionicLegsDecorator _classUnderTest;
+        private static int _measuredSpeed;
+    }
+
     [Subject("Measuring the speed of a running dog")]
     public class When_measuring_the_speed_of_a_running_dog
     {
